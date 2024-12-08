6a6ec18278747df2c1e0f0e43f4981e0fa5affee
duccini <guilhermeduccini@gmail.com>
2024-12-08 12:39:12 -0300

    feat: create Block class

--- Blockchain-and-Cryptocurrency-with-NodeJS/block.js
+++ Blockchain-and-Cryptocurrency-with-NodeJS/block.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp = timestamp;
@@ -15,6 +17,23 @@ class Block {
     Data.....: ${this.data}
     `;
   }
+
+  static genesis() {
+    return new this("Genesis time", "-------", "first-hash", []);
+  }
+
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    // SHA256 retorna um obj
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
 }
 
-module.exports = Block
+module.exports = Block;
